402ca43957e9e1d6635d1c42d0856a37cd9aae83
erockz11 <33884700+erockz11@users.noreply.github.com>
2022-08-15 12:21:27 +1000

    initial commit

---
+++ comp4093/src/main/java/erockz11/comp4093/App.java
@@ -0,0 +1,13 @@
+package erockz11.comp4093;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
