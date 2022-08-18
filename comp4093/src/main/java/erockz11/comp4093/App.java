cec554b71e24bab097410951c6a6f498e75633e0
erockz11 <33884700+erockz11@users.noreply.github.com>
2022-08-18 19:49:05 +1000

    main now loads images from directory

--- comp4093/src/main/java/erockz11/comp4093/App.java
+++ comp4093/src/main/java/erockz11/comp4093/App.java
@@ -1,13 +1,27 @@
 package erockz11.comp4093;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.bytedeco.javacv.CanvasFrame;
+
+
+public class App {
+	
+    public static void main( String[] args ) throws IOException {
+        
+    	BufferedImage img = ImageIO.read(new File("C:\\Users\\Scott\\Documents\\GitHub\\COMP4093\\comp4039\\comp4093\\resources\\images\\img14.jpg"));    	
+    	
+    	CanvasFrame mframe = new CanvasFrame("main");
+    	mframe.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+    	mframe.setCanvasScale(0.5);
+    	mframe.setLocation(0, 0);
+    	mframe.showImage(img);
+    	
     }
+    
 }
