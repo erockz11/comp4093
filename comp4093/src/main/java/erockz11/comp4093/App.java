4a0f5acaa346e34bc88481b9e38b32f52ac543bd
erockz11 <33884700+erockz11@users.noreply.github.com>
2022-08-31 22:58:34 +1000

    implemented thresholding of rgb and hsv images instead of separate hsv channels

--- comp4093/src/main/java/erockz11/comp4093/App.java
+++ comp4093/src/main/java/erockz11/comp4093/App.java
@@ -13,6 +13,7 @@ import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.Java2DFrameUtils;
 import org.bytedeco.javacv.OpenCVFrameConverter.ToIplImage;
 import static org.bytedeco.javacpp.opencv_imgproc.cvtColor;
+import static org.bytedeco.javacpp.opencv_core.bitwise_and;
 
 
 public class App {
@@ -24,51 +25,114 @@ public class App {
     	// Load image
     	BufferedImage src = ImageIO.read(new File("resources/images/img14.jpg"));
     	// Convert BufferedImage to Mat
-    	Mat srcMat = Java2DFrameUtils.toMat(src);
+    	Mat rgbMat = Java2DFrameUtils.toMat(src);
 
     	// Main Frame
     	CanvasFrame mFrame = new CanvasFrame("main");
     	mFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-    	mFrame.setCanvasScale(0.3);
+    	mFrame.setCanvasScale(0.17);
     	mFrame.setLocation(0, 0);
     	mFrame.showImage(src);
 
     	// Convert to HSV
     	Mat hsvMat = new Mat();
-    	cvtColor(srcMat, hsvMat, opencv_imgproc.COLOR_RGB2HSV);
+    	cvtColor(rgbMat, hsvMat, opencv_imgproc.COLOR_RGB2HSV);
 
     	//HSV Frame
     	CanvasFrame hsvFrame = new CanvasFrame("hsv");
     	hsvFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-    	hsvFrame.setCanvasScale(0.3);
-    	hsvFrame.setLocation(1280, 0);
+    	hsvFrame.setCanvasScale(0.17);
+    	hsvFrame.setLocation(640, 0);
     	hsvFrame.showImage(converter.convert(hsvMat));
     	
+    	// Convert to IplImage
+    	IplImage rgbIpl = new IplImage(rgbMat); 
+    	IplImage hsvIpl = new IplImage(hsvMat);
+    	
     	// Split channels
-    	IplImage hsvIpl = new IplImage (hsvMat);
-    	IplImage hueIpl = IplImage.create(hsvIpl.width(), hsvIpl.height(), hsvIpl.depth(), CV_8UC1);
-    	IplImage satIpl = IplImage.create(hsvIpl.width(), hsvIpl.height(), hsvIpl.depth(), CV_8UC1);
-    	IplImage valIpl = IplImage.create(hsvIpl.width(), hsvIpl.height(), hsvIpl.depth(), CV_8UC1);
-    	opencv_core.cvSplit(hsvIpl, hueIpl, satIpl, valIpl, null);
+//    	IplImage hueIpl = IplImage.create(hsvIpl.width(), hsvIpl.height(), hsvIpl.depth(), CV_8UC1);
+//    	IplImage satIpl = IplImage.create(hsvIpl.width(), hsvIpl.height(), hsvIpl.depth(), CV_8UC1);
+//    	IplImage valIpl = IplImage.create(hsvIpl.width(), hsvIpl.height(), hsvIpl.depth(), CV_8UC1);
+//    	opencv_core.cvSplit(hsvIpl, hueIpl, satIpl, valIpl, null);
+    	
+    	// Apply Thresholding
+//    	IplImage hueThreshold = IplImage.create(hsvIpl.width(), hsvIpl.height(), hsvIpl.depth(), CV_8UC1);
+//    	IplImage satThreshold = IplImage.create(satIpl.width(), satIpl.height(), satIpl.depth(), CV_8UC1);
+//    	IplImage valThreshold = IplImage.create(valIpl.width(), valIpl.height(), valIpl.depth(), CV_8UC1);
+    	IplImage rgbThresholdIpl = IplImage.create(rgbIpl.width(), rgbIpl.height(), rgbIpl.depth(), CV_8UC1);
+    	IplImage hsvThresholdIpl = IplImage.create(hsvIpl.width(), hsvIpl.height(), hsvIpl.depth(), CV_8UC1);
+//    	opencv_core.cvInRangeS(hueIpl, opencv_core.cvScalar(100, 100, 100, 0), opencv_core.cvScalar(200, 200, 200, 0), hueThreshold);
+//    	opencv_core.cvInRangeS(satIpl, opencv_core.cvScalar(0, 0, 0, 0), opencv_core.cvScalar(0, 0, 0, 0), satThreshold);
+//    	opencv_core.cvInRangeS(valIpl, opencv_core.cvScalar(0, 0, 0, 0), opencv_core.cvScalar(0, 0, 0, 0), valThreshold);
+    	opencv_core.cvInRangeS(rgbIpl, opencv_core.cvScalar(20, 80, 80, 0), opencv_core.cvScalar(255, 255, 200, 0), rgbThresholdIpl);
+    	opencv_core.cvInRangeS(hsvIpl, opencv_core.cvScalar(20, 80, 80, 0), opencv_core.cvScalar(255, 255, 255, 0), hsvThresholdIpl);
+    	
+    	// Apply smoothing
+    	
+    	
+    	
+    	// Combine Images
+    	Mat combinedThreshold = new Mat();
+    	Mat rgbThresholdMat = opencv_core.cvarrToMat(rgbThresholdIpl);
+    	Mat hsvThresholdMat = opencv_core.cvarrToMat(hsvThresholdIpl);
+    	bitwise_and(rgbThresholdMat, hsvThresholdMat, combinedThreshold);
     	
     	// HSV Channel Frames
-    	CanvasFrame hueFrame = new CanvasFrame("hue");
-    	hueFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-    	hueFrame.setCanvasScale(0.2);
-    	hueFrame.setLocation(0, 855);
-    	hueFrame.showImage(converter.convert(hueIpl));
+//    	CanvasFrame hueFrame = new CanvasFrame("hue");
+//    	hueFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+//    	hueFrame.setCanvasScale(0.17);
+//    	hueFrame.setLocation(0, 500);
+//    	hueFrame.showImage(converter.convert(hueIpl));
+//    	
+//    	CanvasFrame satFrame = new CanvasFrame("saturation");
+//    	satFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+//    	satFrame.setCanvasScale(0.17);
+//    	satFrame.setLocation(640, 500);
+//    	satFrame.showImage(converter.convert(satIpl));
+//    	
+//    	CanvasFrame valFrame = new CanvasFrame("value");
+//    	valFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+//    	valFrame.setCanvasScale(0.17);
+//    	valFrame.setLocation(1280, 500);
+//    	valFrame.showImage(converter.convert(valIpl));
     	
-    	CanvasFrame satFrame = new CanvasFrame("saturation");
-    	satFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-    	satFrame.setCanvasScale(0.2);
-    	satFrame.setLocation(855, 855);
-    	satFrame.showImage(converter.convert(satIpl));
+    	// Threshold Frames
+//    	CanvasFrame hueThresholdFrame = new CanvasFrame("hue threshold");
+//    	hueThresholdFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+//    	hueThresholdFrame.setCanvasScale(0.17);
+//    	hueThresholdFrame.setLocation(0, 940);
+//    	hueThresholdFrame.showImage(converter.convert(hueThreshold));
+//    	
+//    	CanvasFrame satThresholdFrame = new CanvasFrame("saturation threshold");
+//    	satThresholdFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+//    	satThresholdFrame.setCanvasScale(0.17);
+//    	satThresholdFrame.setLocation(640, 940);
+//    	satThresholdFrame.showImage(converter.convert(satThreshold));
+//    	
+//    	CanvasFrame valThresholdFrame = new CanvasFrame("value threshold");
+//    	valThresholdFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+//    	valThresholdFrame.setCanvasScale(0.17);
+//    	valThresholdFrame.setLocation(1280, 940);
+//    	valThresholdFrame.showImage(converter.convert(valThreshold));
     	
-    	CanvasFrame valFrame = new CanvasFrame("value");
-    	valFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-    	valFrame.setCanvasScale(0.2);
-    	valFrame.setLocation(1710, 855);
-    	valFrame.showImage(converter.convert(valIpl));
+    	CanvasFrame rgbThreshFrame = new CanvasFrame("rgb threshold");
+    	rgbThreshFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+    	rgbThreshFrame.setCanvasScale(0.17);
+    	rgbThreshFrame.setLocation(0, 500);
+    	rgbThreshFrame.showImage(converter.convert(rgbThresholdIpl));
+    	
+    	CanvasFrame hsvThreshFrame = new CanvasFrame("hsv threshold");
+    	hsvThreshFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+    	hsvThreshFrame.setCanvasScale(0.17);
+    	hsvThreshFrame.setLocation(640, 500);
+    	hsvThreshFrame.showImage(converter.convert(hsvThresholdIpl));
+    	
+    	CanvasFrame combinedThreshFrame = new CanvasFrame("combined threshold");
+    	combinedThreshFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+    	combinedThreshFrame.setCanvasScale(0.17);
+    	combinedThreshFrame.setLocation(1280, 500);
+    	combinedThreshFrame.showImage(converter.convert(combinedThreshold));
+    	
     }
 
 }
