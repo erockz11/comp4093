d4c9f6222bc2047637e1aa181e35b88af7bedf56
erockz11 <33884700+erockz11@users.noreply.github.com>
2022-10-03 20:55:12 +1100

    updated dependencies

--- comp4093/src/main/java/erockz11/comp4093/App.java
+++ comp4093/src/main/java/erockz11/comp4093/App.java
@@ -2,8 +2,10 @@ package erockz11.comp4093;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+    	
+    	
     }
 
 }
